949a2593de9626512d0a46ce3b90e62ba1c06731
POLETTI Stéphane <polettistephane@msn.com>
2023-09-09 14:15:01 +0200

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  mode: "development",
+  devtool: "source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: '!!html-loader!templates/index.html'
-    })
+      template: "templates/index.html",
+    }),
   ],
-  devtool: 'sourcemap',
-  mode: "development",
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader",
       },
       {
         test: /\.s?css$/,
         exclude: /node_modules/,
-        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: "html-loader",
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: [ '.js', '.jsx' ]
-  }
+    extensions: [".js", ".jsx"],
+    alias: {
+      react: "react", // or the path to your installed React version
+      "react-dom": "react-dom", // or the path to your installed react-dom version
+    },
+  },
 };
